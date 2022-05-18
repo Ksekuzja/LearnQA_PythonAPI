6f6937ea5f46e5821bf226486c09b31e5266b159
kkuz <ksenia.kuz@t-systems.com>
2022-05-18 15:58:16 +0200

    Ex15: Тесты на метод user

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,5 +1,5 @@
 import json.decoder
-
+from datetime import datetime
 from requests import Response
 
 
@@ -19,3 +19,25 @@ class BaseCase:
             assert False, f"Response is not in JSON Format. Response text is '{response.text}'"
         assert name in response_as_dict, f"Response JSON doesn`t have key '{name}'"
         return response_as_dict[name]
+
+    def prepare_registration_data(self, email=None, password=None, username=None, firstname=None, lastname=None):
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        if password is None:
+            password = '123',
+        if username is None:
+            username = f"Username{random_part}"
+        if firstname is None:
+            firstname = f"Firstname{random_part}"
+        if lastname is None:
+            lastname = f"Lastname{random_part}"
+        if email is None:
+            base_part = "learnqa"
+            domain = "example.com"
+            email = f"{base_part}{random_part}@{domain}"
+        return {
+            'password': password,
+            'username': username,
+            'firstName': firstname,
+            'lastName': lastname,
+            'email': email
+        }
